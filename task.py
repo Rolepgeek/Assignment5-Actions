841587b41e77c5bc52199ffca7d1b7e13985cde9
Rolepgeek <rolepgeek@hotmail.com>
2020-03-02 00:39:21 -0800

    Date10

--- task.py
+++ task.py
@@ -17,6 +17,21 @@ def listrun(deck):
 
 
 def daterun(date1, date2):
+    yearday1 = date1.day + monthcalc(date1)
+    yearday2 = date2.day + monthcalc(date2)
+    leapdays = 0
+    for n in range(date1.year, date2.year):
+        if n % 4 == 0:
+            if n % 100 == 0:
+                if n % 400 == 0:
+                    leapdays += 1
+                continue
+            leapdays += 1
+    totaldays = yearday2 - yearday1 + (date2.year - date1.year)*365 + leapdays
+    return abs(totaldays)
+
+
+def monthcalc(date1):
     mon_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     leap_list = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     monthdays1 = 0
@@ -28,34 +43,9 @@ def daterun(date1, date2):
         c1 = 0
     else:
         c1 = 1
-    for n in range(date1.month-1):
+    for n in range(date1.month - 1):
         if c1 == 0:
             monthdays1 += mon_list[n]
         if c1 == 1:
             monthdays1 += leap_list[n]
-    yearday1 = date1.day + monthdays1
-    monthdays2 = 0
-    if date2.year % 4 != 0:
-        c2 = 0
-    elif date2.year % 100 != 0:
-        c2 = 1
-    elif date2.year % 400 != 0:
-        c2 = 0
-    else:
-        c2 = 1
-    for n in range(date2.month - 1):
-        if c2 == 0:
-            monthdays2 += mon_list[n]
-        if c2 == 1:
-            monthdays2 += leap_list[n]
-    yearday2 = date2.day + monthdays2
-    leapdays = 0
-    for n in range(date1.year, date2.year):
-        if n % 4 == 0:
-            if n % 100 == 0:
-                if n % 400 == 0:
-                    leapdays += 1
-                continue
-            leapdays += 1
-    totaldays = yearday2 - yearday1 + (date2.year - date1.year)*365 + leapdays
-    return abs(totaldays)
+    return monthdays1
